a713fcc0bfe741e8953be08bcd2cf47667c504e3
clavinorach <clavinorach@gmail.com>
2024-05-12 01:23:38 +0700

    add css variables with mobile nav

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -7,6 +7,7 @@ import {
     RiTeamFill,
     RiTodoFill,
     RiArrowDownSLine,
+    RiBriefcase4Line,
 } from 'react-icons/ri';
 
 //components
@@ -27,19 +28,51 @@ const Hero = () => {
                 {/* buttons */}
                 <div className='flex flex-col gap-y-3 md:flex-row gap-x-3 mx-auto xl:mx-0 mb-12'>
                     <Link href='/contact'>
-                    <Button className='gap-x-2'>
+                    <Button className='mr-2'>
                         Contact me <Send size={18}/>
                         </Button>
-                    <Button variant='secondary' className='gap-x-2'>
+                    <Button variant='secondary' className='mr-2'>
                         Download CV 
                         <Download size={18}/>
                         <Send size={18}/>
                     </Button>
                     </Link>
                 </div>
+                {/* socials */}
+                <Socials containerStyles='flex gap-x-6 mx-auto xl:mx-0' 
+                iconStyles='text-foreground text-[22px] hover:text-primary transition-all'/>
                 </div>
                 {/* image */}
-                <div className='hidden xl:flex relative'>image</div>
+                <div className='hidden xl:flex relative'>
+                    {/* badge 1*/}
+                    <Badge
+                    containerStyles='absolute top-[24%] -left-[5rem] ' 
+                    icon={<RiBriefcase4Fill />} 
+                    endCountNum={2} 
+                    badgeText='Years Of Experience'
+                    />
+                    {/* badge 2*/}
+                    <Badge
+                    containerStyles='absolute top-[80%] -left-[1rem] ' 
+                    icon={<RiTodoFill />} 
+                    endCountNum={9}
+                    endCountText={'+'}
+                    badgeText='Finished Projects'
+                    />
+                    {/* badge 3*/}
+                    <Badge
+                    containerStyles='absolute top-[55%] -right-8 ' 
+                    icon={<RiTeamFill />} 
+                    endCountNum={9}
+                    endCountText={'k'}
+                    badgeText='Happy Clients'
+                    />
+                    <div className='bg-hero_shape2_light dark:bg-hero_shape2_dark w-[500px] h-[500px] bg-no-repeat absolute -top-1 -right-2'></div>
+                <DevImg 
+                containerStyles='bg-hero_shape w-[510px] w-[462px] bg-no-repeat relative bg-bottom' 
+                // imgSrc=''
+                />
+                </div>
             </div>
             {/* icon */}
             <div className='hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12'>
--- components/Socials.jsx
+++ components/Socials.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+'use client'
 
-const Socials = () => {
+import {
+  RiLinkedinFill, 
+  RiGithubFill,
+  RiInstagramFill
+} from 'react-icons/ri';
+
+import Link from 'next/link';
+
+const icons = [
+  {
+    path: '/',
+    name: <RiLinkedinFill />,
+  },
+  {
+    path: '/',
+    name: <RiGithubFill />,
+  },
+  {
+    path: '/',
+    name: <RiInstagramFill />,
+  },
+]
+
+const Socials = ({containerStyles, iconStyles}) => {
   return (
-    <div>Socials</div>
-  )
+    <div className={containerStyles}>
+      {icons.map((icon, index) => (
+        <Link href={icon.path} key={index}>
+          <div className={iconStyles}>{icon.name}</div>
+        </Link>
+      ))}
+    </div>
+  );
 }
 
 export default Socials
