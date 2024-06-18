6ebf91c92910bebd206ce49cdb9ff09be6b21abc
clavinorach <clavinorach@gmail.com>
2024-06-18 23:35:08 +0700

    merge branch

--- components/About.jsx
+++ components/About.jsx
@@ -95,7 +95,7 @@ const skillData = [
             },
             {
                 imgPath: '/about/figma.svg',
-            },,
+            },
             {
                 imgPath: '/about/notion.svg',
             },
