a964c82e6dcac93690a717284c25bc203ff434c4
clavinorach <clavinorach@gmail.com>
2024-06-30 14:35:25 +0700

    add Projects Page

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,21 +1,23 @@
 import Socials from "./Socials";
 
 const Footer = () => {
-  return <footer className='bg-secondary py-12'>
-    <div className="container mx-auto">
-      <div className='flex flex-col items-center justify-between'>
-        {/* Sosicals */}
-        <Socials 
-        containerStyles='flex gap-x-6 mx-auto xl:mx-0 mb-4' 
-        iconStyles='tex-primary dark:text-white/70 text-[20px] hover:text-white dark:hover:text-primary transition-all'/>
-        {/* Copyright */}
-        <div className='text-muted-foreground'>
-          Copyright &copy; Clavino Ourizqi Rachmadi. All rights reserved.
+  return (
+    <footer className='bg-secondary py-12'>
+      <div className="container mx-auto">
+        <div className='flex flex-col items-center justify-between'>
+          {/* Socials */}
+          <Socials 
+            containerStyles='flex gap-x-6 mx-auto xl:mx-0 mb-4' 
+            iconStyles='text-primary dark:text-white/70 text-[20px] hover:text-white dark:hover:text-primary transition-all'
+          />
+          {/* Copyright */}
+          <div className='text-muted-foreground'>
+            Copyright &copy; Clavino Ourizqi Rachmadi. All rights reserved.
+          </div>
         </div>
       </div>
-    </div>
-  </footer>
-  
+    </footer>
+  )
 }
 
-export default Footer
+export default Footer;
